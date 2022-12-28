3c6e89db021bc7b538327f7c4c5bf0c5e0c57040
tejpratapsingh <tejpratap46@gmail.com>
2022-12-28 10:34:18 +0530

    started file uploader component

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,28 +6,16 @@ import Section from '@/components/section'
 const Index = () => {
 	const videoPlayerRef: RefObject<HTMLVideoElement> = createRef();
 
-	const onFileSelected = (e: any) => {
-		const file = e.target.files[0]
-		const type = file.type
-
-		const fileURL = URL.createObjectURL(file)
-
-		videoPlayerRef.current!.src = fileURL
-
-		console.log(fileURL);
-	}
 
 	return (
 		<Page>
 			<Section>
-				<h2 className='text-xl font-semibold text-zinc-800 dark:text-zinc-200'>
-					<input type="file" name="video file" id="video-file" onChange={onFileSelected} accept="video/*" />
-				</h2>
-
-				<div className='mt-2'>
-					<video className="w-full" controls autoPlay ref={videoPlayerRef}>
-						Your browser does not support the video tag.
-					</video>
+				<div className=" container m-auto grid grid-cols-2">
+					<div className='mt-2'>
+						<video className='w-full' style={{height: '60vh'}} controls autoPlay ref={videoPlayerRef}>
+							Your browser does not support the video tag.
+						</video>
+					</div>
 				</div>
 			</Section>
 		</Page>
