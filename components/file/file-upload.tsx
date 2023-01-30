d6c18d03367762133eb12beb146dffbaab5a10a6
tejpratapsingh <tejpratap46@gmail.com>
2023-01-30 21:54:47 +0530

    added file drag and drop support

--- components/file/file-upload.tsx
+++ components/file/file-upload.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent } from 'react'
-
+import DropZone from '@/components/file/dropzone'
 import Section from '@/components/section'
 
 const FileUpload = (props: FileUploadProps) => {
@@ -15,24 +15,17 @@ const FileUpload = (props: FileUploadProps) => {
 		return (
 			<Section>
 				<input className="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-				type="file"
-				onChange={onFileSelected}
-				accept="video/*" />
+					type="file"
+					onChange={onFileSelected}
+					accept="video/*" />
 			</Section>
 		)
 	} else {
 		return (
 			<Section>
-				<div className="flex items-center justify-center w-full">
-					<label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-						<div className="flex flex-col items-center justify-center pt-5 pb-6">
-							<svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-							<p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-							<p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-						</div>
-						<input id="video-file" type="file" className="hidden" onChange={onFileSelected} accept="video/*" />
-					</label>
-				</div>
+				<DropZone onFileSelected={(file: File) => {
+					props.onFileSelected(file)
+				}} />
 			</Section>
 		)
 	}
